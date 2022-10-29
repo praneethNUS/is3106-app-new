0ce0525690ab587a04186bcd5381866cb7fa1d5a
praneethNUS <shalinda@comp.nus.edu.sg>
2022-10-29 10:04:44 +0800

    Adding files.

---
+++ README.md
@@ -0,0 +1,5 @@
+## IS3106 Sample React Note App
+
+Using functional components and hooks (without using any class components)
+
+Sample React Note App using Create-React-App Structure
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import NoteApp from "./containers/NoteApp";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Welcome to IS3106 Note App</h2>
+      </div>
+      <br />
+      <br />
+      <NoteApp />
+    </div>
+  );
+}
+
+export default App;
